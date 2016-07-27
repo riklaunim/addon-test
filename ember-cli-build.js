507de4ac244dbeaf4a09809292cbc55ed1cf994b
Piotr Maliński <piotr.malinski@socialwifi.com>
2016-07-27 13:14:56 +0200

    Add dependencies.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,12 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
+  app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
+  app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
+  app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+  app.import('bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
+  app.import('bower_components/admin-lte/dist/js/app.min.js');
+  app.import('bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
+  app.import('bower_components/admin-lte/plugins/chartjs/Chart.min.js');
   return app.toTree();
 };
