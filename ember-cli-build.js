8af741536e3370706ffa20ab89f9bc1d3e658c03
Piotr Maliński <piotr.malinski@socialwifi.com>
2016-07-28 14:57:34 +0200

    Add moment.js.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,8 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import('bower_components/moment/moment.js');
+  app.import('vendor/shims/moment.js');
   app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
   app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
   app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   name: 'ember-cli-adminlte',
   included: function(app) {
     this._super.included(app);
+    app.import('bower_components/moment/moment.js');
+    app.import('vendor/shims/moment.js');
     app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
     app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
     app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
