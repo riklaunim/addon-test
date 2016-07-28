26e5f9dfc0ad6726be326407cae9a1c6067c8375
Piotr Maliński <piotr.malinski@socialwifi.com>
2016-07-28 12:52:08 +0200

    Add super call.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 module.exports = {
   name: 'ember-cli-adminlte',
   include: function(app) {
+    this._super.included(app);
     app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
     app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
     app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
