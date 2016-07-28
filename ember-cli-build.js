92acd315ccfea38eee1c1e5b69df3e3be09bee60
Piotr Maliński <piotr.malinski@socialwifi.com>
2016-07-28 12:46:19 +0200

    Make import work when installing as dependency.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,26 @@
 /*jshint node:true*/
 /* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = {
-  name: 'ember-cli-adminlte',
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {
+    // Add options here
+  });
 
-  included: function(app) {
-    this._super.included(app);
-    app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
-    app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
-    app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
-    app.import('bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
-    app.import('bower_components/admin-lte/dist/js/app.min.js');
-    app.import('bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
-    app.import('bower_components/admin-lte/plugins/chartjs/Chart.min.js');
-    app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.js');
-    app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.css');
-  }
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+  app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
+  app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
+  app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+  app.import('bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
+  app.import('bower_components/admin-lte/dist/js/app.min.js');
+  app.import('bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
+  app.import('bower_components/admin-lte/plugins/chartjs/Chart.min.js');
+  app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.js');
+  app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.css');
+  return app.toTree();
 };
--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-adminlte'
+  name: 'ember-cli-adminlte',
+  include: function(app) {
+    app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
+    app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
+    app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+    app.import('bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
+    app.import('bower_components/admin-lte/dist/js/app.min.js');
+    app.import('bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
+    app.import('bower_components/admin-lte/plugins/chartjs/Chart.min.js');
+    app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.js');
+    app.import('bower_components/admin-lte/plugins/daterangepicker/daterangepicker.css');
+  }
 };
