2c2c2c7492cfd005f3ee7122e2492e9064a612be
Piotr Maliński <piotr.malinski@socialwifi.com>
2017-10-17 13:14:41 +0200

    Update paths to yarn installed bower components.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,14 +13,14 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import('bower_components/moment/moment.js');
+  app.import('node_modules/@bower_components/moment/moment.js');
   app.import('vendor/shims/moment.js');
-  app.import('bower_components/admin-lte/bootstrap/css/bootstrap.css');
-  app.import('bower_components/admin-lte/dist/css/skins/_all-skins.css');
-  app.import('bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
-  app.import('bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
-  app.import('bower_components/admin-lte/dist/js/app.min.js');
-  app.import('bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
+  app.import('node_modules/@bower_components/admin-lte/bootstrap/css/bootstrap.css');
+  app.import('node_modules/@bower_components/admin-lte/dist/css/skins/_all-skins.css');
+  app.import('node_modules/@bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+  app.import('node_modules/@bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
+  app.import('node_modules/@bower_components/admin-lte/dist/js/app.min.js');
+  app.import('node_modules/@bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
   app.import('vendor/shims/jquery.js');
   return app.toTree();
 };
