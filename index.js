78f5b93d2648a7ade333650f7241c581d162db86
Piotr Maliński <piotr.malinski@socialwifi.com>
2017-10-18 12:52:42 +0200

    Use npm package paths.

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ module.exports = {
   name: 'ember-cli-adminlte',
   included: function(app) {
     this._super.included(app);
-    app.import('node_modules/@bower_components/moment/moment.js');
+    app.import('node_modules/moment/moment.js');
     app.import('vendor/shims/moment.js');
-    app.import('node_modules/@bower_components/admin-lte/bootstrap/css/bootstrap.css');
-    app.import('node_modules/@bower_components/admin-lte/dist/css/skins/_all-skins.css');
-    app.import('node_modules/@bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
-    app.import('node_modules/@bower_components/admin-lte/bootstrap/js/bootstrap.min.js');
-    app.import('node_modules/@bower_components/admin-lte/dist/js/app.min.js');
-    app.import('node_modules/@bower_components/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
+    app.import('node_modules/admin-lte/bootstrap/css/bootstrap.css');
+    app.import('node_modules/admin-lte/dist/css/skins/_all-skins.css');
+    app.import('node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
+    app.import('node_modules/admin-lte/bootstrap/js/bootstrap.min.js');
+    app.import('node_modules/admin-lte/dist/js/app.min.js');
+    app.import('node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
     app.import('vendor/shims/jquery.js');
   }
 };
